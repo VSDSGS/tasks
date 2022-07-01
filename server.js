acfe81ff3d42cdd25ced90ce1ac008c99a237a5a
vsdsgs <lp9wnandrews@gmail.com>
2022-07-01 15:11:57 +0300

    Added second task

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { join } = require("path");
+
 app.use(bodyParser.json());
 
-const rainVolume = (towers) => {
+const rainAmount = (towers) => {
     let sum = 0;
     let leftMax = towers[0];
     let rightMax = Math.max(...towers.slice(2));
@@ -22,16 +24,37 @@ const rainVolume = (towers) => {
     return sum;
 };
 
+const formatTime = (seconds) => {
+    let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+        res = [];
+
+    if (seconds === 0) return "now";
+    for (let key in time) {
+        if (seconds >= time[key]) {
+            let val = Math.floor(seconds / time[key]);
+            res.push((val += val > 1 ? " " + key + "s" : " " + key));
+            seconds = seconds % time[key];
+        }
+    }
+    return res.length > 1 ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1") : res[0];
+};
+
 app.get("/", (req, res) => {
-    res.send("Send post request with json in it: `towers`: [ numbers] ");
+    res.sendFile(join(__dirname, "../1", "template", "index.html"));
 });
 
-app.post("/", (req, res) => {
+app.post("/towers", (req, res) => {
     const numOfTowers = req.body.towers;
-    sum = rainVolume(numOfTowers);
+    sum = rainAmount(numOfTowers);
     res.json({
         sum,
     });
+});
+
+app.post("/format_time", (req, res) => {
+    const time = req.body.time;
+    const formatedTime = formatTime(time);
+    res.json({ formatedTime });
 });
 
 const port = process.env.PORT || 3000;
